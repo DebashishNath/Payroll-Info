96643507db4a0221d7790dd408b3b168e7b6add7
Debashish Nath <debasis.nath@gmail.com>
2021-04-19 07:12:59 -0400

    Sign In functionality done
    Sign In functionality done

--- UIPay/pay-ui/src/App.js
+++ UIPay/pay-ui/src/App.js
@@ -3,46 +3,46 @@ import { Paper, withStyles, Grid, TextField, Button, FormControlLabel, Checkbox,
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 
-async function doLogin() {
-
-  const requestOptions = {
-    crossDomain:true,
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-        "username" : "Sandip",
-        "password" : "sandip123"
-      })
-  };
-
-  var url='http://192.168.43.241:8086/api/auth/signin';
-  // Storing response
-  const response = await fetch(url,requestOptions);
-  
-  try 
-  {
-    var data = await response.json();
-    alert(data.returnMessage?data.returnMessage.message:'Failed to fetch')
-  }
-  catch(err) {
-    alert(err.message);
-  }
-}
-
-const App=(props) => {
+const App=() => {
   
   const paperStyle={padding:20,height:'60vh',width:280,margin:"20px auto"}
   const avatarStyle={backgroundColor:'green'} 
   const btnStyle={margin:'8px 0'}
   
+  async function doLogin() {
+    
+    const requestOptions = {
+      crossDomain:true,
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+          "username" : document.getElementById("uname").value,
+          "password" : document.getElementById("pwd").value
+        })
+    };
+  
+    var url='http://192.168.43.241:8086/api/auth/signin';
+    // Storing response
+    const response = await fetch(url,requestOptions);
+    
+    try 
+    {
+      var data = await response.json();
+      alert(data.returnMessage?data.returnMessage.message:'Failed to fetch')
+    }
+    catch(err) {
+      alert(err.message);
+    }
+  }
+
   return (
     <Paper elevation={10} style={paperStyle}>
       <Grid align="center">
         <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
         <h2>Sign In</h2>
       </Grid>
-      <TextField label='Username' placeholder='Enter Username' fullWidth required></TextField>
-      <TextField label='Password' placeholder='Enter Password' type='password' fullWidth required></TextField>
+      <TextField id="uname" label='Username' placeholder='Enter Username' fullWidth required></TextField>
+      <TextField id="pwd" label='Password' placeholder='Enter Password' type='password' fullWidth required></TextField>
       <FormControlLabel
         control={
           <Checkbox
@@ -53,7 +53,7 @@ const App=(props) => {
         label="Remember me"
       />
       <Button type='submit' color='primary' variant='contained' style={btnStyle} 
-          fullWidth required>Log In</Button>
+          fullWidth required onClick={() => { doLogin() }}>Log In</Button>
       
       <Typography>
         <Link href="#">Forgot Password ?</Link>
