bc79269efa739e43dde7eb6c864ce114aa95d0f0
Debashish Nath <debasis.nath@gmail.com>
2021-05-11 12:37:41 -0400

    added dept form
    added dept form

--- UIPay/pay-ui/src/Components/LoginComponents/DepartmentForm.js
+++ UIPay/pay-ui/src/Components/LoginComponents/DepartmentForm.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { Table, TextField , Button,Paper } from '@material-ui/core';
 
-class CategoryForm extends Component {
+class DepartmentForm extends Component {
   constructor(props) 
   {
     super(props);
     this.state = {
         accessToken: this.props.history.location.state?.tokenValue
       };
-    this.addCategory = this.addCategory.bind(this);
+    this.addDepartment = this.addDepartment.bind(this);
   }
 
-  async addCategory() {
+  async addDepartment() {
         
         const requestOptions = {
             crossDomain:true,
@@ -19,13 +19,13 @@ class CategoryForm extends Component {
             headers: { 'Content-Type': 'application/json',
                        'Authorization' : 'Bearer ' + this.state.accessToken },
             body: JSON.stringify({
-                "category_id" : "0",
-                "category_code" : document.getElementById("categoryCode").value,
-                "category_name" : document.getElementById("categoryName").value
+                "department_id" : "0",
+                "department_code" : document.getElementById("departmentCode").value,
+                "department_name" : document.getElementById("departmentName").value
               })
         };
       
-      var url='http://192.168.43.241:8086/api/addcategory';
+      var url='http://192.168.43.241:8086/api/adddepartment';
       try 
       {
         const response = await fetch(url,requestOptions);
@@ -51,24 +51,25 @@ class CategoryForm extends Component {
 
         return (
         <div>
-          <Paper elevation={10} style={paperStyle}>
+            <Paper elevation={10} style={paperStyle}>
             <label id = "returnMessage"></label>
             <Table>
-              <tr><td><label>Category Code</label></td>
-                  <td><TextField id="categoryCode" placeholder='Enter Category Code' variant='outlined'></TextField></td>
+              <tr><td><label>Department Code</label></td>
+                  <td><TextField id="departmentCode" placeholder='Enter Department Code' 
+                  variant='outlined'></TextField></td>
               </tr>
-              <tr><td><label>Category Name</label></td>
-                  <td><TextField id="categoryName" placeholder='Enter Category Name' variant='outlined'></TextField></td>
+              <tr><td><label>Department Name</label></td>
+                  <td><TextField id="departmentName" placeholder='Enter Department Name' variant='outlined'></TextField></td>
               </tr>
               <tr><td></td>
                   <td><Button type='submit' color='primary' variant='contained' style={btnStyle} 
-                    onClick={() => { this.addCategory() }}>Save</Button></td>
+                    onClick={() => { this.addDepartment() }}>Save</Button></td>
               </tr>
             </Table>
-          </Paper>
-        </div>
-      );
+            </Paper>
+         </div>
+        );
   }
 }
 
-export default CategoryForm;
+export default DepartmentForm;
