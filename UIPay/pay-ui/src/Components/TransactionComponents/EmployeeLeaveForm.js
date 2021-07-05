a14fab8c7b0527ed1b5f3ef00ba2ab2a30f8d205
Debashish Nath <debasis.nath@gmail.com>
2021-07-05 18:08:22 +0530

    Employee leave form is in progress
    Employee leave form is in progress

--- UIPay/pay-ui/src/Components/TransactionComponents/EmployeeLeaveForm.js
+++ UIPay/pay-ui/src/Components/TransactionComponents/EmployeeLeaveForm.js
@@ -22,11 +22,57 @@ class EmployeeLeaveForm extends PureComponent {
 
     componentDidMount()
     {
-        alert(localStorage.getItem('LeaveApplicationId'));
         var url='http://192.168.43.241:8086/api/leave_types';
         this.populateCombos('LeaveType',url);
         url='http://192.168.43.241:8086/api/employees';
         this.populateCombos('Employee',url);
+        var leaveApplicationId=localStorage.getItem('LeaveApplicationId');
+        this.PopulateLeaveRecord(leaveApplicationId);
+    }
+
+    async PopulateLeaveRecord(leaveApplicationId)
+    {
+        const requestOptions = {
+            crossDomain:true,
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json',
+                        'Authorization' : 'Bearer ' + localStorage.getItem('tokenValue') },
+        };
+        var url='http://192.168.43.241:8086/api/emp_leave/' + leaveApplicationId;
+        const response = await fetch(url,requestOptions);
+        var data = await response.json();
+        if(data!=null)
+        {
+            document.getElementById("EmpCode").innerHTML=data.emp.emp_code;
+            document.getElementById("EmpName").innerHTML=data.emp.emp_first_name + ' ' + data?.emp?.emp_middle_name + ' ' + data.emp.emp_last_name;
+            document.getElementById("ApplicationNo").value=data.leave_application_no;
+            var dateFormat = require('dateformat');
+            document.getElementById("AppDate").value=dateFormat(data.leave_application_date,'yyyy-mm-dd');
+            document.getElementById("FromDate").value=dateFormat(data.from_date,'yyyy-mm-dd');
+            document.getElementById("ToDate").value=dateFormat(data.to_date,'yyyy-mm-dd');
+            let noDays=this.getNoDays(data.from_date,data.to_date);
+            document.getElementById("NoDays").innerHTML=noDays;
+            document.getElementById("ApplicationDetails").value=data.leave_application_details;
+            this.setState({leaveTypeCode : data.leaveType.leave_type_code});
+            this.setState({approveById : data.approvedBy.leave_type_code});
+            alert(data.is_approved);
+            if(data.is_approved === "Y")
+            {
+                this.setState({checked : true});
+            }
+            else
+            {
+                this.setState({checked : false});
+            }
+        }
+    }
+
+    getNoDays(fromDate,toDate)
+    {
+        let diff =  moment(toDate).diff(moment(fromDate));
+        let diffDuration = moment.duration(diff);
+        let noDays=diffDuration.days() + 1;
+        return noDays;
     }
 
     async populateCombos(comboName,url) 
@@ -94,7 +140,7 @@ class EmployeeLeaveForm extends PureComponent {
 
     render()
     {
-        const paperStyle={padding:20,height:'50vh',width:700,margin:"40px 100px",border: '5px solid brown'}
+        const paperStyle={padding:20,height:'75vh',width:700,margin:"40px 100px",border: '5px solid brown'}
         const btnStyle={margin:'8px 0'}
    
         return(
@@ -117,7 +163,7 @@ class EmployeeLeaveForm extends PureComponent {
                     <tr><td><label>Leave Type</label></td>
                         <td>
                         <Select id="leaveTypesCombo" value={this.state.leaveTypeCode} onChange={this.leaveTypeCodeChange}
-                            style={{ border: '1px solid' ,width:'200px' }}>
+                            style={{ border: '1px solid' ,width:'180px' }}>
                             {this.state.leaveTypesToDisplay}
                         </Select>
                         </td>
@@ -137,17 +183,25 @@ class EmployeeLeaveForm extends PureComponent {
                         <td><td><label id ="NoDays"></label></td>
                         </td>
                     </tr><br/>
-                    <tr><td><label>Is Approved</label></td>
+                    <tr>
+                        <td><label>App. Details</label></td>
+                        <td colspan="2"><TextField id="ApplicationDetails" multiline variant='outlined' style={{width :'150%'}}></TextField></td>
+                    </tr><br/>
+                    <tr><td><label>Approved</label></td>
                         <td><input type="checkbox" defaultChecked={this.state.checked}
                             onChange={() => this.setChecked()} /></td>
                         <td><label>Approved By</label></td>
                         <td>
                             <Select id="approvedByCombo" value={this.state.approveById} 
                                         onChange={this.approvedByComboChange}
-                                style={{ border: '1px solid' ,width:'200px' }}>
+                                style={{ border: '1px solid' ,width:'180px' }}>
                                 {this.state.approveByToDisplay}
                             </Select>
                         </td>
+                    </tr><br/>
+                    <tr>
+                        <td><label>Remarks</label></td>
+                        <td colspan="2"><TextField id="Remarks" multiline variant='outlined' style={{width :'150%'}}></TextField></td>
                     </tr><br/>
                     <tr><td></td><td></td><td></td>
                         <td><Button type='submit' color='primary' variant='contained' style={btnStyle} 
