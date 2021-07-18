e304ae8ea6f7986e35e34288ca3c174be68e4f73
Debashish Nath <debasis.nath@gmail.com>
2021-07-18 12:43:00 +0530

    Paysheet Columns and Values have been added
    Paysheet Columns and Values have been added

--- Payroll/src/main/java/com/payroll/payrollWebService/models/payroll/trn_monthly_emp_salary_summary.java
+++ Payroll/src/main/java/com/payroll/payrollWebService/models/payroll/trn_monthly_emp_salary_summary.java
@@ -20,6 +20,8 @@ public class trn_monthly_emp_salary_summary {
     private Double total_earn_amount;
     private Double total_ded_amount;
     private Double net_amount;
+    private String pay_sheet_values;
+    private String pay_sheet_columns;
     private String remarks;
 
     @Transient
@@ -29,7 +31,7 @@ public class trn_monthly_emp_salary_summary {
 
     public trn_monthly_emp_salary_summary(MonthlyEmpSalSummaryIdentity monEmpSalSummaryIdentity,
            mst_company company,Date salary_date,Double total_earn_amount,Double total_ded_amount,
-           Double net_amount,String remarks)
+           Double net_amount,String pay_sheet_values,String pay_sheet_columns,String remarks)
     {
         this.monEmpSalSummaryIdentity =monEmpSalSummaryIdentity;
         this.company=company;
@@ -37,6 +39,8 @@ public class trn_monthly_emp_salary_summary {
         this.total_earn_amount=total_earn_amount;
         this.total_ded_amount=total_ded_amount;
         this.net_amount=net_amount;
+        this.pay_sheet_values=pay_sheet_values;
+        this.pay_sheet_columns=pay_sheet_columns;
         this.remarks=remarks;
     }
 
@@ -86,6 +90,22 @@ public class trn_monthly_emp_salary_summary {
 
     public void setNet_amount(Double net_amount) {
         this.net_amount = net_amount;
+    }
+
+    public String getPay_sheet_columns() {
+        return pay_sheet_columns;
+    }
+
+    public void setPay_sheet_columns(String pay_sheet_columns) {
+        this.pay_sheet_columns = pay_sheet_columns;
+    }
+
+    public String getPay_sheet_values() {
+        return pay_sheet_values;
+    }
+
+    public void setPay_sheet_values(String pay_sheet_values) {
+        this.pay_sheet_values = pay_sheet_values;
     }
 
     public String getRemarks() {
