8fdac4086cc236a7e52b6471af97dddae69e60a8
Debashish Nath <debasis.nath@gmail.com>
2021-07-25 18:49:52 +0530

    update test form
    update test form

--- UIPay/pay-ui/src/Components/ReportComponents/TestForm.js
+++ UIPay/pay-ui/src/Components/ReportComponents/TestForm.js
@@ -1,17 +1,57 @@
 import React from 'react'
-import { Button,Paper } from '@material-ui/core';
+import { Button,Paper,Table,TextField,label } from '@material-ui/core';
 
 class TestForm extends React.Component {
     constructor(props) 
     {
         super(props);
+        this.doEditRecord=this.doEditRecord.bind(this);
+    }
+    
+    validateControls()
+    {
+        if(document.getElementById("code").value.trim().length === 0)
+        {
+            alert("Code cannot be blank");
+            document.getElementById("code").focus();
+            return false;
+        }
+        if(document.getElementById("name").value.trim().length === 0)
+        {
+            alert("Name cannot be blank");
+            document.getElementById("name").focus();
+            return false;
+        }
+        return true;
+    }
+
+    doEditRecord()
+    {
+        if(!this.validateControls())
+        {
+            return;
+        }
+        alert("Record(s) Saved");
     }
     render() { 
-        const paperStyle={padding:20,height:200,width:200,margin:"40px 100px",border: '2px solid brown'}
+        const paperStyle={padding:20,height:200,width:300,margin:"40px 100px",border: '2px solid brown'}
         return(
             <Paper style={paperStyle} variant="outlined">
             <div>
-                <Button color='primary' variant='contained' >Add</Button>
+                <Table>
+                    <tr><td><label>Code</label></td>
+                        <td><TextField id="code" variant='outlined' required inputProps={{ maxLength: 5 }}></TextField></td>
+                    </tr>
+                    <br/>
+                    <tr><td><label>Name</label></td>
+                        <td><TextField id="name" variant='outlined' required inputProps={{ maxLength: 25 }}></TextField></td>
+                    </tr>
+                    <br/>
+                    <tr>
+                        <td></td>
+                        <td><Button color='primary' variant='contained' onClick={() => { this.doEditRecord() }} >Add</Button></td>
+                    </tr>
+                </Table>
             </div>
             </Paper>
         );
